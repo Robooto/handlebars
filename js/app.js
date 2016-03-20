a00fbcd1e72e4f4c6a7c8f7cc96b755997942165
Sam Russell <samrussell63@gmail.com>
2016-03-19 20:10:43 -0700

    inital commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function() {
+    renderPage();
+    
+    function renderPage() {
+        var template = $('#index-template').html();
+        var compiled = Handlebars.compile(template);
+        var rendered = compiled(window.language);
+        $('#main').html(rendered);
+        $('#languageSwitch').click(function() {
+            DogPack.switchLanguage();
+        });
+    }
+})();
